d98e9ddf0167f542f0c612207ffe0cad243bdd78
Luiz Santos <167034236+nandocrowler@users.noreply.github.com>
2025-01-22 22:30:45 -0300

    Add files via upload

---
+++ projeto.js
@@ -0,0 +1,166 @@
+
+
+var pht=document.querySelector("#p1");
+pht.addEventListener("mouseover", function(){    
+    this.style.backgroundColor = "orange"; 
+    
+
+
+var pht=document.querySelector("#p1");
+pht.addEventListener("mouseout", function(){    
+    this.style.backgroundColor = "grey";   
+});
+
+var pht=document.querySelector("#p2");
+pht.addEventListener("mouseover", function(){    
+    this.style.backgroundColor = "orange";   
+});
+var pht=document.querySelector("#p2");
+pht.addEventListener("mouseout", function(){    
+    this.style.backgroundColor = "grey";   
+});
+
+var pht=document.querySelector("#p3");
+pht.addEventListener("mouseover", function(){    
+    this.style.backgroundColor = "orange";   
+});
+var pht=document.querySelector("#p3");
+pht.addEventListener("mouseout", function(){    
+    this.style.backgroundColor = "grey";   
+});
+var pht=document.querySelector("#p4");
+pht.addEventListener("mouseover", function(){    
+    this.style.backgroundColor = "orange";   
+});
+var pht=document.querySelector("#p4");
+pht.addEventListener("mouseout", function(){    
+    this.style.backgroundColor = "grey";   
+});
+var pht=document.querySelector("#p5");
+pht.addEventListener("mouseover", function(){    
+    this.style.backgroundColor = "orange";   
+});
+var pht=document.querySelector("#p5");
+pht.addEventListener("mouseout", function(){    
+    this.style.backgroundColor = "grey";   
+});
+var pht=document.querySelector("#p6");
+pht.addEventListener("mouseover", function(){    
+    this.style.backgroundColor = "orange";   
+});
+var pht=document.querySelector("#p6");
+pht.addEventListener("mouseout", function(){    
+    this.style.backgroundColor = "grey";   
+});
+var pht=document.querySelector("#p7");
+pht.addEventListener("mouseover", function(){    
+    this.style.backgroundColor = "orange";   
+});
+var pht=document.querySelector("#p7");
+pht.addEventListener("mouseout", function(){    
+    this.style.backgroundColor = "grey";   
+});
+var pht=document.querySelector("#p8");
+pht.addEventListener("mouseover", function(){    
+    this.style.backgroundColor = "orange";   
+});
+var pht=document.querySelector("#p8");
+pht.addEventListener("mouseout", function(){    
+    this.style.backgroundColor = "grey";   
+});
+
+var mo = document.querySelector("#second-div");
+mo.addEventListener("mouseover", function(){
+    this.style.cursor = "pointer";
+})
+
+var imagem = document.querySelector("img");
+imagem.addEventListener("mouseover", function(){
+    this.style.cursor = "pointer";
+})
+
+var mais = document.querySelector("#p9");
+mais.addEventListener("click", function(){
+    alert('evento');
+})})
+
+
+// var dc = document.getElementById("website-title");
+// console.log(dc);
+
+
+// Tentando Inserir evento de click em javascript
+// const button = document.querySelector("#btn-evento");
+// button.addEventListener("click", function(){
+//     this.style.backgroundColor = "red";
+
+//     var buttt = document.querySelector("html");
+//     buttt.addEventListener("click", function(){
+//         this.style.backgroundColor = "orange";
+//     })
+// });
+
+// const button2 = document.querySelector("#btn-evento");
+// button.addEventListener("dblclick", function(){
+//     this.style.backgroundColor = "black";
+//     this.style.color = "white";
+
+//     var buttt = document.querySelector("html");
+//     buttt.addEventListener("dblclick", function(){
+//         this.style.backgroundColor = "white";
+//     })
+// });
+
+
+// const mudar = document.querySelector("html");
+// mudar.addEventListener("click", function(){
+//     this.style.backgroundColor='orange';
+// })
+// const mudar2 = document.querySelector("html");
+// mudar.addEventListener("dblclick", function(){
+//     this.style.backgroundColor='white';
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const buttons = document.querySelectorAll("#btn-evento button")
+
+// buttons.forEach((btn)=>{
+//     btn.addEventListener("click", (e)=>{
+//         const value = e.target.innerText;
+//     })
+// })
+
+
+
+
+
+
+
+
+
+
+
+// const btt = document.querySelector("#btn-evento");
+// btt.addEventListener("click", function(){
+//     const value = btt;
+// })
+
+
---
+++ script.js
@@ -0,0 +1,46 @@
+// var dc1 = document.getElementById("website-title")
+// dc1.innerHTML = "SAGITTARIUS A*"
+
+
+// Tentando Inserir evento de click em javascript
+// const button = document.querySelector("#btn-evento");
+// button.addEventListener("click", function(){
+//     this.style.backgroundColor = "red";
+
+//     var buttt = document.querySelector("html");
+//     buttt.addEventListener("click", function(){
+//         this.style.backgroundColor = "orange";
+//     })
+// });
+
+// const button2 = document.querySelector("#btn-evento");
+// button.addEventListener("dblclick", function(){
+//     this.style.backgroundColor = "black";
+//     this.style.color = "white";
+
+//     var buttt = document.querySelector("html");
+//     buttt.addEventListener("dblclick", function(){
+//         this.style.backgroundColor = "white";
+//     })
+// });
+
+function change(){
+    
+const darkmod = document.getElementById("escuro");
+console.log(darkmod);
+
+const mudar = document.querySelector("html");
+mudar.style.backgroundColor = "orange";
+
+};
+
+function release(){
+    const reset = document.getElementById("claro");
+    console.log(reset);
+
+    const back = document.querySelector("html");
+    back.style.backgroundColor = "white";
+  
+
+}
+
